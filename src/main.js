af16bfdf880e5dbf039fa59626746ceceff9e06b
William Cameron <william@williamcameron.co.uk>
2019-10-04 20:42:15 +0100

    add vue composition api

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import VueCompositionApi from '@vue/composition-api';
+
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi); 
 
 new Vue({
   router,
